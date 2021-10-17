d1a0db9cd5136073df7172d54163344a9a196db1
Jacob Weinstock <jakobweinstock@gmail.com>
2021-10-16 21:30:09 -0600

    Add ipxe binary builds:
    This is needed in order to embed the ipxe binaries in
    the Go binary.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 BINARY:=ipxe
 OSFLAG:= $(shell go env GOHOSTOS)
+OSARCH:= $(shell go env GOHOSTOS)
 BUILD_ARGS:=GOARCH=amd64 CGO_ENABLED=0 go build -trimpath -ldflags '-s -w -extldflags "-static"'
 
 help: ## show this help message
-	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[32m%-30s\033[0m %s\n", $$1, $$2}'
+	@grep -E '^[a-zA-Z_-]+.*:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[32m%-30s\033[0m %s\n", $$1, $$2}'
 
 .PHONY: test
-test: ## Run unit tests
+test: ## run unit tests
 	go test -v -covermode=count ./...
 
 .PHONY: darwin
@@ -18,12 +19,36 @@ linux: ## compile for linux
 	GOOS=linux ${BUILD_ARGS} -o bin/${BINARY}-linux-amd64 main.go
 
 .PHONY: build
-build: ## compile the binary for the native OS
+build: binary ## compile the binary for the native OS
 ifeq (${OSFLAG},linux)
 	@$(MAKE) linux
 else
 	@$(MAKE) darwin
 endif
+
+binary: binary/ipxe.efi binary/snp.efi binary/undionly.kpxe ## build all upstream ipxe binaries
+
+ipxe-sha: ## get shasum of ipxe source code archive
+
+# ipxe_sha_or_tag := v1.21.1 # could not get this tag to build ipxe.efi
+# https://github.com/ipxe/ipxe/tree/2265a65191d76ce367913a61c97752ab88ab1a59
+ipxe_sha_or_tag := "2265a65191d76ce367913a61c97752ab88ab1a59"
+ipxe_build_in_docker := $(shell if [ $(OSARCH) = "darwin" ]; then echo true; else echo false; fi)
+
+binary/ipxe.efi: ## build ipxe.efi
+	scripts/build_ipxe.sh bin-x86_64-efi/ipxe.efi "$(ipxe_sha_or_tag)" $(ipxe_build_in_docker) $@
+
+binary/undionly.kpxe: ## build undionly.kpxe
+	scripts/build_ipxe.sh bin/undionly.kpxe "$(ipxe_sha_or_tag)" $(ipxe_build_in_docker) $@
+
+binary/snp.efi: ## build snp.efi
+	scripts/build_ipxe.sh bin-arm64-efi/snp.efi "$(ipxe_sha_or_tag)" $(ipxe_build_in_docker) $@ "CROSS_COMPILE=aarch64-unknown-linux-gnu-"
+
+.PHONY: binary/clean
+binary/clean: ## clean all ipxe binaries, upstream ipxe source code directory, and ipxe source tarball
+	rm -rf binary/ipxe.efi binary/snp.efi binary/undionly.kpxe
+	rm -rf upstream-*
+	rm -rf ipxe-*
 
 # BEGIN: lint-install /Users/jacobweinstock/repos/jacobweinstock/ipxe
 # http://github.com/tinkerbell/lint-install
---
+++ scripts/build_ipxe.sh
@@ -0,0 +1,121 @@
+#!/usr/bin/env bash
+
+set -eux
+
+function download_ipxe_repo() {
+    local sha_or_tag="$1"
+    if [ ! -f "ipxe-${sha_or_tag}.tar.gz" ]; then
+        echo "downloading"
+        curl -fLo ipxe-"${sha_or_tag}".tar.gz https://github.com/ipxe/ipxe/archive/${sha_or_tag}.tar.gz
+    else
+        echo "already downloaded"
+    fi
+    
+}
+
+function extract_ipxe_repo() {
+    local archive_name="$1"
+    local archive_dir="$2"
+
+    if [ ! -d "$archive_dir" ]; then
+        echo "extracting"
+        mkdir -p "${archive_dir}"
+        tar -xzf "${archive_name}" -C "${archive_dir}" --strip-components 1
+    else
+        echo "already extracted"
+    fi
+}
+
+function build_ipxe() {
+    local ipxe_dir="$1"
+    local ipxe_bin="$2"
+    local run_in_docker="$3"
+    local env_opts="$4"
+    local embed_path="$5"
+
+    if [ "${run_in_docker}" = true ]; then
+        if [ ! -f "${ipxe_dir}/src/${ipxe_bin}" ]; then
+            echo "running in docker"
+            docker run -it --rm -v ${PWD}:/code -w /code nixos/nix nix-shell scripts/shell.nix --run "${env_opts} make -C ${ipxe_dir}/src EMBED=${embed_path} ${ipxe_bin}"
+        fi
+    else
+        echo "running locally"
+        ${env_opts} make -C "${ipxe_dir}/src" EMBED=${embed_path} ${ipxe_bin}
+    fi
+}
+
+function mv_embed_into_build() {
+    local embed_path="$1"
+    local ipxe_dir="$2"
+
+    cp -a "${embed_path}" "${ipxe_dir}"/src/embed.ipxe
+}
+
+function make_local_empty() {
+    local ipxe_dir="$1" 
+
+    rm -rf "${ipxe_dir}"/src/config/local/*
+}
+
+function copy_common_files() {
+    local ipxe_dir="$1" 
+    cp -a scripts/ipxe-customizations/colour.h "${ipxe_dir}"/src/config/local/
+    cp -a scripts/ipxe-customizations/common.h "${ipxe_dir}"/src/config/local/
+    cp -a scripts/ipxe-customizations/console.h "${ipxe_dir}"/src/config/local/
+}
+
+function copy_custom_files() {
+    local ipxe_dir="$1"
+    local ipxe_bin="$2"
+
+    case "${ipxe_bin}" in
+    bin/undionly.kpxe)
+    	cp scripts/ipxe-customizations/general.undionly.h "${ipxe_dir}"/src/config/local/general.h
+    	;;
+    bin/ipxe.lkrn)
+    	cp scripts/ipxe-customizations/general.undionly.h "${ipxe_dir}"/src/config/local/general.h
+    	;;
+    bin-x86_64-efi/ipxe.efi)
+    	cp scripts/ipxe-customizations/general.efi.h "${ipxe_dir}"/src/config/local/general.h
+        cp scripts/ipxe-customizations/isa.h "${ipxe_dir}"/src/config/local/isa.h
+    	;;
+    bin-arm64-efi/snp.efi)
+    	cp scripts/ipxe-customizations/general.efi.h "${ipxe_dir}"/src/config/local/general.h
+    	;;
+    *) echo "unknown binary: ${ipxe_bin}" >&2 && exit 1 ;;
+    esac
+}
+
+function customize_aarch_build() {
+    local ipxe_dir="$1"
+    # http://lists.ipxe.org/pipermail/ipxe-devel/2018-August/006254.html
+    sed -i.bak '/^WORKAROUND_CFLAGS/ s|^|#|' "${ipxe_dir}"/src/arch/arm64/Makefile
+}
+
+function customize() {
+    local ipxe_dir="$1"
+    local ipxe_bin="$2"
+
+    make_local_empty "${ipxe_dir}"
+    copy_common_files "${ipxe_dir}"
+    copy_custom_files "${ipxe_dir}" "${ipxe_bin}"
+    customize_aarch_build "${ipxe_dir}"
+}
+
+function main() {
+    local bin_path="$(echo $1 | xargs)"
+    local ipxe_sha_or_tag="$(echo $2 | xargs)"
+    local ipxe_build_in_docker="$(echo $3 | xargs)"
+    local final_path="$(echo $4 | xargs)"
+    local env_opts="$(echo $5 | xargs)"
+    local embed_path="$(echo $6 | xargs)"
+
+    download_ipxe_repo "${ipxe_sha_or_tag}"
+    extract_ipxe_repo "ipxe-${ipxe_sha_or_tag}.tar.gz" "upstream-${ipxe_sha_or_tag}"
+    mv_embed_into_build "${embed_path}" "upstream-${ipxe_sha_or_tag}"
+    customize "upstream-${ipxe_sha_or_tag}" "${bin_path}"
+    build_ipxe "upstream-${ipxe_sha_or_tag}" "${bin_path}" "${ipxe_build_in_docker}" "${env_opts}" "embed.ipxe"
+    cp -a "upstream-${ipxe_sha_or_tag}/src/${bin_path}" "${final_path}"
+}
+
+main "$1" "$2" "$3" "$4" "${5:-''}" "${6:-scripts/embed.ipxe}" 
